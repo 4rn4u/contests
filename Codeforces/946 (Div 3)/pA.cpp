879e9f415886edc1607e7d88b48834ce01c195a5
4rn4u <vilx00@protonmail.com>
2024-05-24 09:16:25 +0200

    asd

--- Codeforces/946 (Div 3)/pA.cpp
+++ Codeforces/946 (Div 3)/pA.cpp
@@ -14,13 +14,9 @@ int main() {
         while (y > 0 || x > 0) {
             screens++;
             int available_cells = 15;
-
-            // Place up to 2 2x2 icons (each takes 4 cells)
             int place_2x2 = min(y, 2);
             y -= place_2x2;
             available_cells -= place_2x2 * 4;
-
-            // Place as many 1x1 icons as possible in the remaining cells
             int place_1x1 = min(x, available_cells);
             x -= place_1x1;
         }
